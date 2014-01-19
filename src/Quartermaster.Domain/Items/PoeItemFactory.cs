681c7bfab8eada67987c9785e32209a4bf7f7df3
mathematically <matthewrbnsn@gmail.com>
2014-01-19 11:48:05 +0000

    Added exception handling and some bare bones logging.

--- src/Quartermaster.Domain/Items/PoeItemFactory.cs
+++ src/Quartermaster.Domain/Items/PoeItemFactory.cs
@@ -1,9 +1,13 @@
+using System;
 using Mathematically.Quartermaster.Domain.Parser;
+using NLog;
 
 namespace Mathematically.Quartermaster.Domain.Items
 {
     public class PoeItemFactory : IPoeItemFactory
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
         private readonly IPoeItemParser _itemParser;
 
         public PoeItemFactory(IPoeItemParser itemParser)
@@ -13,7 +17,14 @@ namespace Mathematically.Quartermaster.Domain.Items
 
         public IPoeItem CreateItem(string gameItemText)
         {
-            _itemParser.Parse(gameItemText);
+            try
+            {
+                _itemParser.Parse(gameItemText);
+            }
+            catch (Exception e)
+            {
+                Log.ErrorException("Failed to parse clipboard text " + gameItemText + " (" + e.Message + ")", e);
+            }
 
             return new PoeItem(_itemParser);
         }
--- src/Quartermaster/App.xaml.cs
+++ src/Quartermaster/App.xaml.cs
@@ -1,11 +1,25 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
+using NLog;
 
 namespace Mathematically.Quartermaster
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            Dispatcher.UnhandledException += DispatcherOnUnhandledException;
+        }
+
+        private void DispatcherOnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            args.Handled = true;
+            Log.ErrorException("Unhandled exception", args.Exception);
+        }
     }
 }
